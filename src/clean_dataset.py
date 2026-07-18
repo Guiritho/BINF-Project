1bad024c51a112debe802d521a2533863d0dfc42
Clovis Thoreton <clovis.thoreton@epita.fr>
2026-07-18 20:48:00 +0200

    feat: palier 1

--- src/clean_dataset.py
+++ src/clean_dataset.py
@@ -1,4 +1,6 @@
 import sys
+import numpy as np
+from csv_utils import build_output_path, read_matrix_csv, write_matrix_csv
 
 
 def select_top_variance_genes(data, gene_count):
@@ -6,7 +8,10 @@ def select_top_variance_genes(data, gene_count):
     fluctuent pas beaucoup entre observations. Pour ce faire : calculer la variance de chaque
     gène et garder uniquement les N gènes avec la plus forte variance.
     """
-    pass
+    variances = np.var(data, axis=0)
+    highest_variance_indices = np.argsort(variances)[::-1][:gene_count]
+
+    return highest_variance_indices
 
 
 def clean_dataset(datadir, dataname, gene_count):
@@ -14,7 +19,18 @@ def clean_dataset(datadir, dataname, gene_count):
     dossier contenant les données, dataname le nom du fichier csv contenant les données brutes
     et K est le nombre de gènes a conserver.
     """
-    pass
+    input_path = build_output_path(datadir, dataname)
+    header, row_ids, data = read_matrix_csv(input_path)
+
+    selected_indices = select_top_variance_genes(data, gene_count)
+    cleaned_header = [header[index] for index in selected_indices]
+    cleaned_data = data[:, selected_indices]
+
+    output_filename = f"{dataname}_cleaned_{gene_count}.csv"
+    output_path = build_output_path(datadir, output_filename)
+    output_decimals = 5
+
+    write_matrix_csv(output_path, cleaned_header, row_ids, cleaned_data, output_decimals)
 
 
 def main():
